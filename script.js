99637b4a900ccfd0157bf32a18602b09c0a1b997
petemurray45 <pmurray697@gmail.com>
2023-10-19 23:41:19 +0100

    added cloned tile function with form entries

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ closeFormButton.forEach(button => {
 })
 
 closeTileButton.forEach(button => {
-    button.addEventListener('click', () => {
-        closeTile();
+    button.addEventListener('click', (e) => {
+        e.parentNode.remove();
     })
 })
 
@@ -71,6 +71,8 @@ form.addEventListener('submit', (e) => {
     console.log(newObject);
     gamesPlayed.push(newObject);
     document.forms[0].reset();
+
+    createTile(newObject);
 
     closeForm();
     
@@ -134,9 +136,54 @@ function newObj(){
 
 //// creates tiles with form elements displayed in a tile
 
-function createTile(){
+function createTile(object){
 
-    console.log(this.title());
+    const newTile = document.createElement('div');
+    newTile.classList.add('gameTile');
+
+    mainBodyElement.appendChild(newTile);
+
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('tileCloseButton');
+    closeButton.innerHTML = '&times;'
+    closeButton.setAttribute('data', 'close-tile');
+    newTile.appendChild(closeButton);
+
+    newTile.appendChild(closeButton);
+
+    const newGameTitle = document.createElement('p');
+    newGameTitle.classList.add('gameTitle');
+    newGameTitle.innerText = object.title;
+    newTile.appendChild(newGameTitle);
+
+    const genreText = document.createElement('p');
+    genreText.classList.add('gameGenre');
+    genreText.innerText = object.genre;
+    newTile.appendChild(genreText);
+
+    const gamePlayTime = document.createElement('p');
+    gamePlayTime.classList.add('gamePlayTime');
+    gamePlayTime.innerText = object.timePlayed;
+    newTile.appendChild(gamePlayTime);
+
+    const gamePlayed = document.createElement('p');
+    gamePlayed.classList.add('havePlayed');
+    newTile.appendChild(gamePlayed);
+
+
+    const gamePlayedText = document.createElement('span');
+    gamePlayedText.innerText = 'Played:'
+    gamePlayed.appendChild(gamePlayedText);
+
+    const switchContainer = document.createElement('div');
+    const switctLabel = document.createElement('label');
+    const switchDiv = document.createElement('div');
+    switctLabel.classList.add('switch');
+    switchContainer.classList.add('switchContainer');
+
+
+
+
     
     
 }
